3aa5cda098a83999dc2f16e87dc99f853dc61556
Denys Tsybko <72654542+edexmusic@users.noreply.github.com>
2021-03-25 21:32:42 +0200

    Delete jemsy directory

--- jemsy/med.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByTagName('video')[0].volume = 0.1;
-
-function showIt() {
-    document.getElementById("div1").style.visibility = "visible";
-    document.getElementById("div2").style.visibility = "visible";
-  }
-  setTimeout("showIt()", 15500);
-
-
